45086a2bb4c0f21f3ca310feb70eab167e48960c
Nick <nmarks99@gmail.com>
2022-12-05 11:17:16 -0600

    screenshot now automatically copies to clipboard!

--- bin/screenshot.py
+++ bin/screenshot.py
@@ -9,4 +9,18 @@ stamp = datetime.datetime.now()
 stamp = stamp.strftime("%m-%d-%Y_%I-%M-%S")
 name = "".join([screenshot_dir_path,"screenshot_",stamp,".png"])
 
-subprocess.call(["import",name]) # screenshot with imagemagick
+# screenshot with imagemagick
+subprocess.call(["import",name])
+
+# Get the most recent screenshot
+result = subprocess.Popen(
+    ["ls -t /home/nick/Pictures/Screenshots/*.png | head -1"],
+    shell=True,
+    stdout=subprocess.PIPE
+)
+file = result.stdout.read().decode("utf-8")
+file = file.rstrip('\n')
+assert result.wait() == 0
+result = subprocess.Popen([f"cat {file} | xclip -selection clipboard -target image/png -i"],shell=True)
+assert result.wait() == 0
+
