fb5f7e8436c83ccf8f2f07e7502b21acb04a7f46
Nick Marks <nmarks99@gmail.com>
2023-05-30 12:33:31 -0500

    minor updates

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,13 +1,16 @@
 
+-- handles transparent background
 vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 require("catppuccin").setup({
     transparent_background = false
 })
 
-vim.cmd [[colorscheme catppuccin]]
-
--- use this to set transparent background if needed
+-- use this to set transparent background if needed (non-catpuccin)
 -- vim.cmd([[
     -- autocmd vimenter * hi Normal guibg=NONE ctermbg=NONE
     -- autocmd vimenter * hi NonText guibg=NONE ctermbg=NONE
 -- ]])
+
+
+-- sets the default theme
+vim.cmd [[colorscheme gruvbox]]
