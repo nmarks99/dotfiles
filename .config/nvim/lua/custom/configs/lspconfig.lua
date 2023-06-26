642dc6f10edf64965d1659e469e3f9d69f4b73f3
Nick Marks <nmarks99@gmail.com>
2023-06-26 09:51:59 -0500

    add custom nvchad config

---
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,36 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+  "html",
+  "cssls",
+  "clangd",
+  "rust_analyzer",
+  "jedi_language_server"
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- rust-analyzer
+lspconfig.rust_analyzer.setup({
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml")
+})
+
+-- Without the loop, you would have to manually set up each LSP 
+-- 
+-- lspconfig.html.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+-- lspconfig.cssls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "jedi-langauge-server"
+      }
+    }
+  }
+
+}
+return plugins
+
