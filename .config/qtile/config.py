1a917dc36e5fc0aa61a7950dfb34bfda890997a1
Nick Marks <nmarks99@gmail.com>
2023-11-21 09:02:10 -0600

    update qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -84,37 +84,6 @@ keys = [
     )
 ]
 
-##############
-#### Bar #####
-##############
-
-soft_sep = {'linewidth': 2, 'size_percent': 70,
-            'foreground': '393939', 'padding': 7}
-
-icon_theme_path = '/usr/share/icons/AwOkenWhite/clear/24x24/status/'
-main_bar = bar.Bar(
-            [
-                widget.GroupBox(),
-                widget.Mpris2(background='253253', name='spotify',
-                              stop_pause_text='▶', scroll_chars=None,
-                              display_metadata=['xesam:title', 'xesam:artist'],
-                              objname="org.mpris.MediaPlayer2.spotify"),
-                widget.Sep(linewidth=2, size_percent=100, padding=12),
-                widget.Prompt(),
-                widget.Volume(theme_path=icon_theme_path),
-                widget.WindowName(),
-                widget.Systray(),
-                widget.Sep(**soft_sep),
-                widget.BatteryIcon(theme_path=icon_theme_path),
-                widget.Battery(foreground='247052', low_percentage=0.20,
-                               low_foreground='fa5e5b', update_delay=10,
-                               format='{percent:.0%} {hour:d}:{min:02d} '
-                                      '{watt:.2}W'),
-                widget.Sep(**soft_sep),
-                widget.Clock(timezone='Europe/Paris', format='%B %-d, %H:%M'),
-            ], 30)
-
-
 #################
 #### Groups #####
 #################
@@ -163,33 +132,64 @@ layouts = [
 #########################
 
 widget_defaults = dict(
-    font="JetBrainsMono",
-    fontsize=12,
+    font="JetBrains Mono Bold",
+    fontsize=13,
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        wallpaper=desktop_wallpaper,
+        wallpaper = desktop_wallpaper,
         wallpaper_mode="fill",
 
-        top=bar.Bar(
+        top = bar.Bar(
             [
-                # widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
+                widget.GroupBox(
+                    rounded=True,
+                    disable_drag=True,
+                 ),
+
                 widget.Spacer(bar.STRETCH),
-                widget.Clock(format="%a %m-%d-%Y"),
+
+                # =========================================
+
+                widget.Clock(
+                    format="%a %m-%d-%Y",
+                    font="JetBrains Mono Bold",
+                    fontsize=13
+                ),
                 widget.Sep(padding=10),
-                widget.Clock(format="%I:%M:%S %p"),
+                widget.Clock(
+                    format="%I:%M:%S %p",
+                    font="JetBrains Mono Bold",
+                    fontsize=13
+                ),
+
                 widget.Spacer(bar.STRETCH),
-                widget.Battery(format="󰁺 {percent:2.0%} "),
+
+                # =========================================
+
+                widget.BatteryIcon(
+                    theme_path='~/.config/qtile/assets/battery/',
+                    scale=1,
+                ),
+
+                widget.Battery(
+                    font='JetBrains Mono Bold',
+                    format='{percent:2.0%}',
+                ),
+
                 widget.Sep(padding=10),
-                widget.Wlan(interface="wlp115s0", format=" \t", disconnected_message="󰖪 \t",),
-                # widget.QuickExit(),
+
+                widget.Wlan(
+                        interface="wlp115s0",
+                        format=" \t",
+                        disconnected_message="󰖪 \t",
+                ),
+
             ],
-            24,
+            28,
             ),
         )
 ]
