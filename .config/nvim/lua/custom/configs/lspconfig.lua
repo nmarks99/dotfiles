17ab1feccde34be1872ca36f2175b2207ed392cc
nmarks99 <nmarks@anl.gov>
2023-12-20 16:30:55 -0600

    add ruff-lsp for python, remove null-ls

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -5,47 +5,41 @@ local lspconfig = require "lspconfig"
 local servers = {
     "html",
     "cssls",
+    "jedi_language_server"
 }
 
 -- attach all servers that don't have specific settings
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
 
 -- rust-analyzer
 lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"rust"},
-  root_dir = lspconfig.util.root_pattern("Cargo.toml")
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"rust"},
+    root_dir = lspconfig.util.root_pattern("Cargo.toml")
 })
 
 -- clangd
 lspconfig.clangd.setup({
-  filetypes = {"c", "cpp", "h", "hpp"},
-  on_attach = on_attach,
-  capabilities = capabilities
-})
-
-lspconfig.pyright.setup ({
+    filetypes = {"c", "cpp", "h", "hpp"},
     on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = {"pyright-langserver", "--stdio"},
-    filetypes = {"python"},
-    single_file_support = true,
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = false,
-                typeCheckingMode = "on",
-            },
-        },
-    },
+    capabilities = capabilities
 })
 
 
+local on_attach_ruff = function(client, bufnr)
+    client.server_capabilities.hoverProvider = false
+end
+lspconfig.ruff_lsp.setup({
+    on_attach = on_attach_ruff,
+    init_options = {
+        settings = {
+            args = {},
+        }
+    }
+})
