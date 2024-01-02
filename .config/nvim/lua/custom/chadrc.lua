0439a16a7675258dbd63cca323592c7e757094ee
nmarks99 <nmarks@anl.gov>
2024-01-02 09:26:23 -0600

    switch from nvchad to custom nvim config

--- .config/nvim/lua/custom/chadrc.lua
+++
@@ -1,8 +0,0 @@
----@type ChadrcConfig 
-local M = {}
-M.ui = {theme = 'ayu_dark', transparency=true}
-M.plugins = 'custom.plugins'
-M.mappings = require "custom.mappings"
-vim.opt.softtabstop = 4
-return M
-
--- .config/nvim/lua/custom/configs/cmp.lua
+++
@@ -1,8 +0,0 @@
-local cmp = require "plugins.configs.cmp"
-local options = {
-  mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-  }
-}
-return options
